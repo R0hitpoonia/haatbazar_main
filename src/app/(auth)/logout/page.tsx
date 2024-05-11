592fbe8207595fbb5c2b5fd40e310ae272060baa
R0hitpoonia <rohitpoonia.cse23@jecrc.ac.in>
2024-05-11 15:48:16 +0530

    register changes

--- src/app/(auth)/logout/page.tsx
+++ src/app/(auth)/logout/page.tsx
@@ -1,11 +1,14 @@
 "use client";
 import { toast } from "@/components/ui/use-toast";
 import { useLogoutUserMutation } from "@/redux/api/authApi";
+import { authSlice, setAuthState, setAuthUser } from "@/redux/slice/authSlice";
+import { useAppDispatch } from "@/redux/store";
 import { redirect } from "next/navigation";
 import React, { useEffect } from "react";
 
 const Logout = () => {
   const [logout, { isSuccess }] = useLogoutUserMutation();
+  const dispatch = useAppDispatch();
   useEffect(() => {
     logout();
     localStorage.removeItem("token");
@@ -15,8 +18,19 @@ const Logout = () => {
         variant: "default",
         duration: 2000,
       });
-      redirect("/");
     }
+    dispatch(setAuthState(false));
+    dispatch(
+      setAuthUser({
+        email: "",
+        name: "",
+        address: [],
+        password: "",
+        profilePhoto: "",
+        role: "user",
+      }),
+    );
+    redirect("/");
   }, []);
 };
 
